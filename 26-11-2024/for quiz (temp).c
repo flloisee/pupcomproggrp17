e3b705afc01722fedba16abbb49fc2897675e080
flloisee <llego.fl06@gmail.com>
2024-11-27 18:11:40 +0800

    27/11/2024 upd Quiz 2 Code

---
+++ 26-11-2024/for quiz (temp).c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+
+int main() {
+    int letterCode;
+    float letterWeight, cost;
+    while (1) {
+        printf("Enter the weight of the letter in grams: ");
+        scanf("%f", &letterWeight);
+        printf("\nLetter code 1 is ordinary, while letter code 2 is special.\n\nEnter the letter's code: ");
+        scanf("%d", &letterCode);
+
+        if (letterCode > 2 || letterCode < 1) {
+            printf("Invalid Input!");
+        } else {
+            if (letterCode == 2) { // SE
+                if (letterWeight <= 1) {
+                    printf("Cost: P40.00");
+                } else if (letterWeight > 1) {
+                    cost = 40 + (25 * (letterWeight - 1));
+                    printf("Cost: P%.2f", cost);
+                }
+            } else if (letterCode == 1) { // STD
+                if (letterWeight <= 1) {
+                    printf("Cost: P20.00");
+                } else if (letterWeight > 1) {
+                    cost = 20 + (15 * (letterWeight - 1));
+                    printf("Cost: P%.2f", cost);
+                }
+            }
+        }
+
+        getchar();
+        char repeat;
+        printf("\n\nAgain? Y or N ");
+        scanf("%c", &repeat);
+        if (repeat == 'Y' || repeat == 'y') {
+
+        } else {
+            printf("See you soon!");
+            break;
+        }
+    }
+
+    return 0;
+}
